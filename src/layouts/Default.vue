0dce88c30e272cb047802ab56a8c63ebb3e107dc
Nick Grove <nickgrove.se@gmail.com>
2024-02-13 15:08:33 -0500

    Footer and Layout Changed

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -18,7 +18,6 @@
     <footer>
         <a class="footer-element" href="https://nickgrove.netlify.app/">My Portfolio Site</a>
         <a class="footer-element" href="https://github.com/NickGroveSE">Github</a>
-        <a class="footer-element" >Buy Me a Coffee :)</a>
     </footer>
 
 </template>
@@ -53,12 +52,12 @@
     /* Main Styling */
     #main {
         width: 100%;
-        height: calc(100vh - 120px);
+        height: calc(100vh - 180px);
     }
 
     /* Footer Styling */
     footer {
-        height: 60px;
+        height: 90px;
         width: 100%;
         margin: auto 0;
         text-align: center;
@@ -67,7 +66,7 @@
     .footer-element {
         vertical-align: top;
         margin: 10px 10px 10px 10px;
-        line-height: 40px;
+        line-height: 60px;
         display: inline-block;
         color: var(--text);
         
